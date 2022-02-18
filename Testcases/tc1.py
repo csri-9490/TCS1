8dbdb238b3ac34a69d2f34d994cd254fa6df0154
srikanth <srikanthchelukala@gmail.com>
2022-02-18 14:43:05 +0530

    tcs1

--- Testcases/tc1.py
+++ Testcases/tc1.py
@@ -2,14 +2,22 @@ from selenium import  webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 class sample:
     driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     def a1(self):
         self.driver.get("https://www.facebook.com/")
-        self.driver.find_element(By.XPATH,"//input[@id='email']")
-        self.driver.find_element(By.XPATH,"//input[@id='pass']")
-        self.driver.find_element(By.CSS_SELECTOR,"#u_0_h_aG")
+        self.driver.find_element(By.XPATH,"//input[@id='email']").send_keys("reddy.csri@gmail.com")
+        self.driver.find_element(By.XPATH,"//input[@id='pass']").send_keys("Csrireddy77!")
+        self.driver.implicitly_wait(7)
+        try:
+            self.driver.find_element(By.XPATH,"//button[@id='u_0_h_aG']").click()
+        except Exception as e:
+            print("no  major issue")
 
     def a2(self):
         self.driver.get("https://www.jetbrains.com/help/pycharm/set-up-a-git-repository.html#clone-repo")
+a=sample()
+a.a1()
+a.a2()
